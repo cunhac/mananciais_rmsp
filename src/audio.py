cd8e65305b73afe7e2e6acb8497ed13662420b96
cunhac <cunhacarol1621@gmail.com>
2021-12-06 15:02:53 -0300

    print audio

--- src/audio.py
+++ src/audio.py
@@ -4,12 +4,15 @@ from playsound import playsound
 
 def produzir_audio(volume_total):
 
-    audio_principal= f'O volume total de água armazenado hoje na Região Metropolitana de São Paulo é de {volume_total} por cento.'
+    audio_principal= f'Bom dia. O volume total de água armazenado hoje na Região Metropolitana de São Paulo é de {volume_total} por cento.'
     
     if float(volume_total.replace(',', '.')) < 60. :
-        audio_final= f'{audio_principal} Faça a sua parte! Economize mais água'
+        audio_final= f'{audio_principal} Faça a sua parte! Economize mais água.'
+        print(audio_final)
     else:
         audio_final= audio_principal
+        print(audio_final)
+        
 
     frase = gTTS(audio_final, lang='pt')
     frase.save('frase.mp3')
